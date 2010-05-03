276319f83017927b1cae70f61baa1baa0aaa39cc
maldoxD <maldoxd@gmail.com>
2010-05-03 20:00:04 +0000

    ultima version cuadrantes

--- tesis/trunk/jmetal/util/Spea2FitnessNew.java
+++ tesis/trunk/jmetal/util/Spea2FitnessNew.java
@@ -16,13 +16,13 @@ import java.util.*;
  */
 public class Spea2FitnessNew {
 
+    private Problem problem_;
     private int maxdistance = 0;
     /**
      * Stores the distance between solutions
      */
-    private double[][] distanceObj = new double[2][2];
-    double cuadrante[] = new double[2];
-    private double[] distanceObjMayor = null;
+    double cuadrante_[] = null;
+    private double[][] distanceObj = null;
     /**
      * Stores the solutionSet to assign the fitness
      */
@@ -45,14 +45,21 @@ public class Spea2FitnessNew {
      * Creates a new instance of Spea2Fitness for a given <code>SolutionSet</code>.
      * @param solutionSet The <code>SolutionSet</code>
      */
-    public Spea2FitnessNew(SolutionSet solutionSet) {
+    public Spea2FitnessNew(SolutionSet solutionSet, Problem problem) {
+        this.problem_ = problem;
+        double[] cuadrante = new double[problem.getNumberOfObjectives()];
+        this.cuadrante_ = cuadrante;
         solutionSet_ = solutionSet;
+        double[][] distanceObj2 = new double[problem_.getNumberOfObjectives()][2];
+        distanceObj = distanceObj2;
         for (int i = 0; i < solutionSet_.size(); i++) {
             solutionSet_.get(i).setLocation(i);
         } // for
-// distance = distance_.distanceMatrix(solutionSet);
-        distanceObj[0][1] = solutionSet.get(0).getObjective(0);
-        distanceObj[1][1] = solutionSet.get(0).getObjective(1);
+        for (int i = 0; i < problem_.getNumberOfObjectives(); i++) {
+
+            distanceObj[i][0] = solutionSet.get(0).getObjective(i);
+            distanceObj[i][1] = solutionSet.get(0).getObjective(i);
+        }
         for (int j = 0; j < solutionSet.size(); j++) {
             for (int i = 0; i < solutionSet.get(i).numberOfObjectives(); i++) {
                 if (solutionSet.get(j).getObjective(i) < distanceObj[i][0]) {
@@ -72,9 +79,9 @@ public class Spea2FitnessNew {
     public void fitnessAssign() {
         double[] strength = new double[solutionSet_.size()];
         double[] rawFitness = new double[solutionSet_.size()];
-
-        cuadrante[0] = (distanceObj[0][1] - distanceObj[0][0]) / 4;
-        cuadrante[1] = (distanceObj[1][1] - distanceObj[1][0]) / 4;
+        for (int i = 0; i < cuadrante_.length; i++) {
+            cuadrante_[i] = (distanceObj[i][1] - distanceObj[i][0]) / 4;
+        }
         //Calculate the strength value
         // strength(i) = |{j | j <- SolutionSet and i dominate j}|
         for (int i = 0; i < solutionSet_.size(); i++) {
@@ -96,14 +103,27 @@ public class Spea2FitnessNew {
                     if (dominance_.compare(solutionSet_.get(i), solutionSet_.get(j)) == 1) {
                         rawFitness[i] += strength[j];
                     } // if
-                }}
-//             else {
-//                rawFitness[i] = strength[i];
-//            }
+                }
+            } else if (strength[i] < solutionSet_.size() / 2) {
+                for (int j = 0; j < solutionSet_.size(); j++) {
+
+                    if (dominance_.compare(solutionSet_.get(i), solutionSet_.get(j)) == 1) {
+                        rawFitness[i] += strength[j];
+                    } // if
+                }
+            } else {
+                rawFitness[i] =  solutionSet_.size() - strength[i];
+            }
 
             // for
-        } // for
 
+            solutionSet_.get(i).setFitness(rawFitness[i]);
+        }// for
+//        for (int i = 0; i < solutionSet_.size(); i++) {
+//            if (lugar(solutionSet_.get(i)) == 1) {
+//                //    System.out.println(rawFitness[i] + " " + strength[i]);
+//            }
+//        }
 
         // Add the distance to the k-th individual. In the reference paper of SPEA2,
         // k = sqrt(population.size()), but a value of k = 1 recommended. See
@@ -119,7 +139,7 @@ public class Spea2FitnessNew {
 //
 //              kDistance = 1.0 / (k + 2.0); // Calcule de D(i) distance
 //      //population.get(i).setFitness(rawFitness[i]);
-//      solutionSet_.get(i).setFitness(rawFitness[i] + kDistance);
+
 //    } // for
     } // fitnessAsign
 
@@ -135,13 +155,11 @@ public class Spea2FitnessNew {
         double d = distanceObj[1][1];
         for (int i = 0; i < solution1.numberOfObjectives(); i++) {
             value1 = solution1.getObjective(i);
-
-            if (((value1 < distanceObj[i][1]) && (value1 > distanceObj[i][1] - cuadrante[i])) || ((value1 > distanceObj[i][0]) && (value1 < distanceObj[i][0] + cuadrante[i]))) {
+            if (((value1 > distanceObj[i][1] - cuadrante_[i])) || (value1 < distanceObj[i][0] + cuadrante_[i])) {
                 flag = 1;
             } else {
                 flag = -1;
             }
-
             if (flag == 1) {
                 obj = 1;
             }
@@ -258,3 +276,4 @@ public class Spea2FitnessNew {
         return aux;
     } // environmentalSelection
 } // Spea2Fitness
+
