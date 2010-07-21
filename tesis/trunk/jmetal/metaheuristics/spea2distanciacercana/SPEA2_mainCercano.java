8e327f0b2157a7a78b9c3d93dbc7e1aed2e7e220
maldoxd <maldoxd@9db0af5f-ee38-f6d2-8adf-ea2f5bddf6d8>
2010-07-21 01:26:09 +0000

    ALgoritmo final con la distancia

---
+++ tesis/trunk/jmetal/metaheuristics/spea2distanciacercana/SPEA2_mainCercano.java
@@ -0,0 +1,131 @@
+/**
+ * SPEA2_main.java
+ *
+ * @author Juan J. Durillo
+ * @version 1.0
+ */
+package jmetal.metaheuristics.spea2distanciacercana;
+
+import java.io.IOException;
+import jmetal.base.*;
+import jmetal.base.operator.crossover.*   ;
+import jmetal.base.operator.mutation.*    ; 
+import jmetal.base.operator.selection.*   ;
+import jmetal.problems.*                  ;
+
+import jmetal.qualityIndicator.QualityIndicator;
+
+import jmetal.util.Configuration;
+import jmetal.util.JMException;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import jmetal.qualityIndicator.Hypervolume;
+import jmetal.qualityIndicator.SetCoverage;
+import jxl.read.biff.BiffException;
+import jxl.write.WriteException;
+
+public class SPEA2_mainCercano {
+  public static Logger      logger_ ;      // Logger object
+  public static FileHandler fileHandler_ ; // FileHandler object
+  
+  /**
+   * @param args Command line arguments. The first (optional) argument specifies 
+   *             the problem to solve.
+   * @throws JMException 
+   * @throws IOException 
+   * @throws SecurityException 
+   * Usage: three options
+   *      - jmetal.metaheuristics.mocell.MOCell_main
+   *      - jmetal.metaheuristics.mocell.MOCell_main problemName
+   *      - jmetal.metaheuristics.mocell.MOCell_main problemName ParetoFrontFile
+   */
+  public static void main(String [] args) throws JMException, IOException, ClassNotFoundException, WriteException, BiffException {
+    Problem   problem   ;         // The problem to solve
+    Algorithm algorithm ;         // The algorithm to use
+    Operator  crossover ;         // Crossover operator
+    Operator  mutation  ;         // Mutation operator
+    Operator  selection ;         // Selection operator
+        
+    QualityIndicator indicators ; // Object to get quality indicators
+
+    // Logger object and file to store log messages
+    logger_      = Configuration.logger_ ;
+    fileHandler_ = new FileHandler("SPEA2.log"); 
+    logger_.addHandler(fileHandler_) ;
+    
+    indicators = null ;
+    if (args.length == 1) {
+      Object [] params = {"Real"};
+      problem = (new ProblemFactory()).getProblem(args[0],params);
+    } // if
+    else if (args.length == 2) {
+      Object [] params = {"Real"};
+      problem = (new ProblemFactory()).getProblem(args[0],params);
+      indicators = new QualityIndicator(problem, args[1]) ;
+    } // if
+    else { // Default problem
+//     problem = new Kursawe("Real", 3);
+     // problem = new Tanaka("Real");
+      //problem = new ZDT1("ArrayReal", 1000);
+      //problem = new ZDT4("BinaryReal");
+      //problem = new WFG1("Real");
+      //problem = new DTLZ1("Real");
+      //problem = new OKA2("Real") ;
+       problem = new KnapsackMuchos("Real",5);
+    } // else
+       
+    algorithm = new SPEA2Cercano(problem);
+    
+    // Algorithm parameters
+    algorithm.setInputParameter("populationSize",100);
+    algorithm.setInputParameter("archiveSize",100);
+    algorithm.setInputParameter("maxEvaluations",25000);
+      
+    // Mutation and crossover for real codification
+//    crossover = CrossoverFactory.getCrossoverOperator("SBXCrossover");
+    crossover = CrossoverFactory.getCrossoverOperator("SinglePointCrossover");
+    crossover.setParameter("probability",0.9);                   
+    crossover.setParameter("distributionIndex",20.0);
+   mutation = MutationFactory.getMutationOperator("BitFlipMutation");
+//    mutation = MutationFactory.getMutationOperator("PolynomialMutation");
+    mutation.setParameter("probability",1.0/problem.getNumberOfVariables());
+    mutation.setParameter("distributionIndex",20.0);
+        
+    // Selection operator 
+    selection = SelectionFactory.getSelectionOperator("BinaryTournament") ;                           
+    algorithm.setInputParameter("indicators",indicators) ;
+
+    // Add the operators to the algorithm
+    algorithm.addOperator("crossover",crossover);
+    algorithm.addOperator("mutation",mutation);
+    algorithm.addOperator("selection",selection);
+    
+    // Execute the algorithm
+    long initTime = System.currentTimeMillis();
+    SolutionSet population = algorithm.execute();
+    long estimatedTime = System.currentTimeMillis() - initTime;
+
+    // Result messages 
+    logger_.info("Total execution time: "+estimatedTime + "ms");
+    logger_.info("Objectives values have been writen to file FUNspeaProm");
+     logger_.info("Variables values have been writen to file VAR");
+  
+    population.printVariablesToFile("VAR","SPEA2Cercano");
+    population.printObjectivesToFile("FUNspeaCercano","SPEA2Cercano");
+     Hypervolume toma = new Hypervolume();
+     SetCoverage T = new SetCoverage();
+        @SuppressWarnings("static-access")
+     double[][] f = T.archivoAMatriz("FUNspea");
+  //  double HV = toma.hypervolume(population.writeObjectivesToMatrix(),f, problem.getNumberOfObjectives());
+    //System.out.print("Este es el HV del problema: "+HV);
+
+    if (indicators != null) {
+      logger_.info("Quality indicators") ;
+      logger_.info("Hypervolume: " + indicators.getHypervolume(population)) ;
+      logger_.info("GD         : " + indicators.getGD(population)) ;
+      logger_.info("IGD        : " + indicators.getIGD(population)) ;
+      logger_.info("Spread     : " + indicators.getSpread(population)) ;
+      logger_.info("Epsilon    : " + indicators.getEpsilon(population)) ;
+    } // if 
+  }//main
+} // SPEA2_main.java
