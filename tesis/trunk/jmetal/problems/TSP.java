f33a17db125649220e191fd21f1aae4ec2a41a9c
ajaarreazav <ajaarreazav@9db0af5f-ee38-f6d2-8adf-ea2f5bddf6d8>
2010-09-04 21:24:21 +0000

    TSPv1

--- tesis/trunk/jmetal/problems/TSP.java
+++ tesis/trunk/jmetal/problems/TSP.java
@@ -19,15 +19,14 @@ import java.io.*;
  */
 public class TSP extends Problem {
 
-    public int numberOfCities_ = 12;
+    public static int numberOfCities_ = 12;
     public double ciudadInicio_;
-    double[][] matrizDistancia_ = new double[numberOfCities_][numberOfCities_];
-    double[][] matrizGasolina_ = new double[numberOfCities_][numberOfCities_];
-    double[][] matrizX_ = new double[numberOfCities_][numberOfCities_];
-    /**
-     * Creates a new TSP problem instance. It accepts data files from TSPLIB
-     * @param filename The file containing the definition of the problem
-     */
+    private double[][] matrizDistancia_;
+    private double[][] matrizGasolina_;
+    private double[][] matrizX1;
+    private double[][] matrizX2;
+    private double[][] matrizX3;
+
     public TSP(double ciudadInicio, int numeroDeObjetivos) throws FileNotFoundException, IOException, ClassNotFoundException {
 
         numberOfVariables_ = 1;
@@ -35,17 +34,17 @@ public class TSP extends Problem {
         numberOfConstraints_ = 0;
         ciudadInicio_ = ciudadInicio;
         problemName_ = "TSP";
-        readProblem("C:/tesis/Matriz Distancia.txt", matrizDistancia_);
-        readProblem("C:/tesis/Matriz Gasolina.txt", matrizGasolina_);
-        readProblem("C:/tesis/Matriz X.txt", matrizX_);
+        matrizDistancia_ = readProblem("C:/tesis/TSP porblems/Distancia.tsp", matrizDistancia_);
+        matrizGasolina_ = readProblem("C:/tesis/TSP porblems/Gasolina.tsp", matrizGasolina_);
+        matrizX1 = readProblem("C:/tesis/TSP porblems/MatrizX1.tsp", matrizX1);
+        matrizX2 = readProblem("C:/tesis/TSP porblems/MatrizX2.tsp", matrizX2);
+        matrizX3 = readProblem("C:/tesis/TSP porblems/MatrizX3.tsp", matrizX3);
 
         solutionType_ = new PermutationSolutionType(this);
         variableType_ = new Class[numberOfVariables_];
         length_ = new int[1];
         variableType_[0] = Class.forName("jmetal.base.variable.Permutation");
         length_[0] = numberOfCities_;
-
-
     } // TSP
 
     public void evaluate(Solution solution) {
@@ -69,49 +68,176 @@ public class TSP extends Problem {
         double distanciaRecorrida = 0;
         double gasolinaGastada = 0;
         double xCosa = 0;
+        double xCosa2 = 0;
+        double xCosa3 = 0;
         for (int i = 0; i < ciudades.length; i++) {
             if ((i + 1) < ciudades.length) {
                 distanciaRecorrida += matrizDistancia_[ciudades[i]][ciudades[i + 1]];
                 gasolinaGastada += matrizGasolina_[ciudades[i]][ciudades[i + 1]];
-            switch(numberOfObjectives_){
-                case 3:
-                    xCosa += matrizX_[ciudades[i]][ciudades[i + 1]];
+                if (numberOfObjectives_ == 3) {
+                    xCosa += matrizX1[ciudades[i]][ciudades[i + 1]];
+                } else if (numberOfObjectives_ == 4) {
+                    xCosa += matrizX1[ciudades[i]][ciudades[i + 1]];
+                    xCosa2 += matrizX2[ciudades[i]][ciudades[i + 1]];
+                } else if (numberOfObjectives_ == 5) {
+                    xCosa += matrizX1[ciudades[i]][ciudades[i + 1]];
+                    xCosa2 += matrizX2[ciudades[i]][ciudades[i + 1]];
+                    xCosa3 += matrizX3[ciudades[i]][ciudades[i + 1]];
                 }
             } else {
                 distanciaRecorrida += matrizDistancia_[ciudades[i]][ciudades[0]];
                 gasolinaGastada += matrizGasolina_[ciudades[i]][ciudades[0]];
-                switch(numberOfObjectives_){
-                case 3:
-                    xCosa += matrizX_[ciudades[i]][ciudades[0]];
+                if (numberOfObjectives_ == 3) {
+                    xCosa += matrizX1[ciudades[i]][ciudades[0]];
+                } else if (numberOfObjectives_ == 4) {
+                    xCosa += matrizX1[ciudades[i]][ciudades[0]];
+                    xCosa2 += matrizX2[ciudades[i]][ciudades[0]];
+                } else if (numberOfObjectives_ == 5) {
+                    xCosa += matrizX1[ciudades[i]][ciudades[0]];
+                    xCosa2 += matrizX2[ciudades[i]][ciudades[0]];
+                    xCosa3 += matrizX3[ciudades[i]][ciudades[0]];
                 }
             }
         }
-        solution.setObjective(0, distanciaRecorrida);
-        solution.setObjective(1, gasolinaGastada);
-        solution.setObjective(2, xCosa);
+        solution.setObjective(0, gasolinaGastada);
+        solution.setObjective(1, distanciaRecorrida);
+        switch (numberOfObjectives_) {
+            case 3:
+                solution.setObjective(2, xCosa);
+                break;
+            case 4:
+                solution.setObjective(2, xCosa);
+                solution.setObjective(3, xCosa2);
+                break;
+            case 5:
+                solution.setObjective(2, xCosa);
+                solution.setObjective(3, xCosa2);
+                solution.setObjective(4, xCosa3);
+                break;
+        }
     } // evaluate
 
-    public static void readProblem(String fileName, double[][] matriz) throws FileNotFoundException, IOException {
-        int fila = 0;
-        String linea = "";
-        FileInputStream fis = new FileInputStream(fileName);
-        InputStreamReader isw = new InputStreamReader(fis);
-        BufferedReader br = new BufferedReader(isw);
-        try {
-            while (br.ready()) {
-                linea = br.readLine();
-                String[] filanumeros = linea.split(" ");
-                String[] numeros = filanumeros[0].split(",");
-                for (int i = 0; i < numeros.length; i++) {
-                    matriz[fila][i] = Integer.parseInt(numeros[i].toString());
-                }
-                fila++;
+    private double[][] readProblem(String fileName, double[][] matriz) throws FileNotFoundException, IOException {
 
+        Reader inputFile = new BufferedReader(
+                new InputStreamReader(
+                new FileInputStream(fileName)));
+        StreamTokenizer token = new StreamTokenizer(inputFile);
+
+
+
+
+        try {
+            boolean found;
+            found = false;
+            token.nextToken();
+
+
+
+
+            while (!found) {
+                if ((token.sval != null) && ((token.sval.compareTo("DIMENSION") == 0))) {
+                    found = true;
+
+
+
+
+                } else {
+                    token.nextToken();
+
+
+
+
+                }
+            } // while
+            token.nextToken();
+            token.nextToken();
+            numberOfCities_ = (int) token.nval;
+            matriz = new double[numberOfCities_][numberOfCities_];
+            found = false;
+            token.nextToken();
+
+
+
+
+            while (!found) {
+                if ((token.sval != null)
+                        && ((token.sval.compareTo("SECTION") == 0))) {
+                    found = true;
+
+
+
+
+                } else {
+                    token.nextToken();
+
+
+
+
+                }
             }
-        } catch (Exception e) {
+            double[] c = new double[2 * numberOfCities_];
+
+
+
+
+            for (int i = 0; i
+                    < numberOfCities_; i++) {
+                token.nextToken();
+
+
+
+
+                int j = (int) token.nval;
+                token.nextToken();
+                c[
+
+2 * (j - 1)] = token.nval;
+                token.nextToken();
+                c[
+
+2 * (j - 1) + 1] = token.nval;
+
+
+
+
+            } // for
+            double dist;
+
+
+
+
+            for (int k = 0; k
+                    < numberOfCities_; k++) {
+                matriz[k][k] = 0;
+
+
+
+
+                for (int j = k + 1; j
+                        < numberOfCities_; j++) {
+                    dist = Math.sqrt(Math.pow((c[k * 2] - c[j * 2]), 2.0)
+                            + Math.pow((c[k * 2 + 1] - c[j * 2 + 1]), 2));
+                    dist = (int) (dist + .5);
+                    matriz[k][j] = dist;
+                    matriz[j][k] = dist;
+
+
+
+
+                } // for
+            } // for
+        } // try
+        catch (Exception e) {
             System.err.println("TSP.readProblem(): error when reading data file " + e);
             System.exit(1);
-        }
-    }
-} // TSP
 
+
+
+
+        } // catch
+        return matriz;
+
+
+    }
+}
