6eabe2005666858d7f46903ec85acdf82cc1568d
maldoxd <maldoxd@9db0af5f-ee38-f6d2-8adf-ea2f5bddf6d8>
2010-09-27 00:28:14 +0000

    probelma empaquetado

--- tesis/trunk/jmetal/qualityIndicator/PruebasVarias.java
+++ tesis/trunk/jmetal/qualityIndicator/PruebasVarias.java
@@ -13,6 +13,7 @@ import jmetal.util.Configuration;
 import jmetal.util.JMException;
 import java.util.logging.FileHandler;
 import java.util.logging.Logger;
+import jmetal.metaheuristics.nsgaII.NSGAII;
 import jmetal.metaheuristics.spea2.SPEA2;
 import jmetal.metaheuristics.spea2distanciacercana.SPEA2Cercano;
 import jxl.read.biff.BiffException;
@@ -44,28 +45,46 @@ public class PruebasVarias {
 
         try {
             parametros();
-            int corrida = 1;
+            int corrida = 30;
 
-
+            long[] estimatedTimeCercano = new long[corrida];
+            long[] estimatedTimeSpea = new long[corrida];
+            long[] estimatedTimeNsga = new long[corrida];
             for (int i = 0; i < corrida; i++) {
                 long initTime = System.currentTimeMillis();
 
-
-                algoritmo(0);
-                population = algorithm.execute();
-                population.printVariablesToFile("VAR", "SPEA2"+i);
-                population.printObjectivesToFile("FUNspea" + i, "SPEA2"+i);
                 algoritmo(1);
                 population = algorithm.execute();
-                population.printVariablesToFile("VAR", "SPEA2Cercano"+i);
-                population.printObjectivesToFile("FUNspeaCercano" + i, "SPEA2Cercano"+i);
+                String a = algorithm.toString();
+                population.printVariablesToFile(problem.getName() + " " + population.get(0).numberOfObjectives() +" "+ population.get(0).numberOfVariables()+ "/SPEACercano/VAR_" + i, "SPEA2Cercano" + i);
+                population.printObjectivesToFile(problem.getName() + " " + population.get(0).numberOfObjectives() +" "+population.get(0).numberOfVariables()+ "/SPEACercano/FUNspeaCercano_" + i, "SPEA2Cercano" + i);
 
-                long estimatedTime = (System.currentTimeMillis() - initTime) / 1000;
-                System.out.println(estimatedTime + " tiempo transcurrido");
+                estimatedTimeCercano[i] = (System.currentTimeMillis() - initTime) / 1000;
+                System.out.println(estimatedTimeCercano[i] + " tiempo transcurrido CERCANO");
+                initTime = System.currentTimeMillis();
+                algoritmo(0);
+                population = algorithm.execute();
+                population.printVariablesToFile(problem.getName() + " " + population.get(0).numberOfObjectives() +" "+population.get(0).numberOfVariables()+ "/SPEA2/VAR_" + i, "SPEA2" + i);
+                population.printObjectivesToFile(problem.getName() + " " + population.get(0).numberOfObjectives() +" "+ population.get(0).numberOfVariables()+"/SPEA2/FUNspea2_" + i, "SPEA2" + i);
+                estimatedTimeSpea[i] = (System.currentTimeMillis() - initTime) / 1000;
+                System.out.println(estimatedTimeSpea[i] + " tiempo transcurrido SPEA2");
+                initTime = System.currentTimeMillis();
+                algoritmo(2);
+                population = algorithm.execute();
+                population.printVariablesToFile(problem.getName() + " " + population.get(0).numberOfObjectives() +" "+ population.get(0).numberOfVariables()+"/NSGA2/VAR_" + i, "SPEA2" + i);
+                population.printObjectivesToFile(problem.getName() + " " + population.get(0).numberOfObjectives() +" "+population.get(0).numberOfVariables()+ "/NSGA2/FUNnsga2_" + i, "nsga2" + i);
+                estimatedTimeNsga[i] = (System.currentTimeMillis() - initTime) / 1000;
+                System.out.println(estimatedTimeNsga[i] + " tiempo transcurrido NSGA2");
 
             }
-            SetCoverage covertura = new SetCoverage();
-               covertura.main(args);
+            population.printTimeToFile(problem.getName() + " " + population.get(0).numberOfObjectives() + " "+population.get(0).numberOfVariables()+"/SPEACercano/tiempoCorridas", estimatedTimeCercano);
+            population.printTimeToFile(problem.getName() + " " + population.get(0).numberOfObjectives() +" "+ population.get(0).numberOfVariables()+"/SPEA2/tiempoCorridas", estimatedTimeSpea);
+            population.printTimeToFile(problem.getName() + " " + population.get(0).numberOfObjectives() +" "+population.get(0).numberOfVariables()+ "/NSGA2/tiempoCorridas", estimatedTimeNsga);
+            String[] parametros = new String[2];
+            parametros[0]=(problem.getName() + " " + population.get(0).numberOfObjectives()+" "+population.get(0).numberOfVariables());
+            parametros[1]= corrida+"";
+            SetCoveragevariasCorridas covertura = new SetCoveragevariasCorridas();
+            covertura.main(parametros);
             System.out.println("");
         } catch (Exception e) {
             e.printStackTrace();
@@ -86,8 +105,10 @@ public class PruebasVarias {
     public static void algoritmo(int a) {
         if (a == 0) {
             algorithm = new SPEA2(problem);
-        } else {
+        } else if (a == 1) {
             algorithm = new SPEA2Cercano(problem);
+        } else {
+            algorithm = new NSGAII(problem);
         }
 
         // Algorithm parameters
@@ -117,13 +138,13 @@ public class PruebasVarias {
             logger_.addHandler(fileHandler_);
             indicators = null;
             // Default problem
-    
+
 //             problem = new Tanaka("Real");
             //problem = new ZDT1("ArrayReal", 1000);
             //problem = new ZDT4("BinaryReal");
             //problem = new WFG1("Real");
             //problem = new DTLZ1("Real");
-            
+
 
 
 //kursawe
@@ -131,19 +152,21 @@ public class PruebasVarias {
 //    mutation = MutationFactory.getMutationOperator("PolynomialMutation");
 //    crossover = CrossoverFactory.getCrossoverOperator("SBXCrossover");
             // Mutation and crossover for real codification
-     //TSP
-//                problem = new TSP(0, 3);
+////     TSP
+//                problem = new TSP(0, 5);
 //    crossover = CrossoverFactory.getCrossoverOperator("TwoPointsCrossover");
 //    mutation = MutationFactory.getMutationOperator("SwapMutation");
 //
-         problem = new KnapsackMuchos("Real",3) ;
+////
+//            problem = new KnapsackMuchos("Real", 6);
+                         problem = new SetPacking(3) ;
             crossover = CrossoverFactory.getCrossoverOperator("SinglePointCrossover");
             mutation = MutationFactory.getMutationOperator("BitFlipMutation");
-    
+
             crossover.setParameter("probability", 0.9);
             crossover.setParameter("distributionIndex", 20.0);
             //knapsack
-            
+
             mutation.setParameter("probability", 1.0 / problem.getNumberOfVariables());
             mutation.setParameter("distributionIndex", 20.0);
 
@@ -155,3 +178,4 @@ public class PruebasVarias {
         }
     }
 } // SPEA2_main.java
+
--- tesis/trunk/jmetal/qualityIndicator/SetCoveragevariasCorridas.java
+++ tesis/trunk/jmetal/qualityIndicator/SetCoveragevariasCorridas.java
@@ -5,99 +5,139 @@
 package jmetal.qualityIndicator;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import jmetal.base.SolutionSet;
 import jmetal.metaheuristics.spea2.SPEA2_main;
+import java.lang.Object;
+import jxl.read.biff.BiffException;
+import jxl.write.WriteException;
 
 /**
  *
  * @author Amir
  */
 public class SetCoveragevariasCorridas {
-
+public static  SolutionSet poblacion;
     @SuppressWarnings("static-access")
-    public static void main(String[] args) {
-        int corrida=1;
-        double[][] dominanciaSpea=new double[corrida][corrida];
-        double[][] dominanciaCercano=new double[corrida][corrida];
-        double[][] HV=new double[2][corrida];
-          Hypervolume toma = new Hypervolume();
-        for (int i =0;i<corrida;i++){
+    public static void main(String[] args) throws FileNotFoundException, IOException, WriteException, BiffException {
+
+        poblacion = new SolutionSet();
+        int corrida =30;//Integer.parseInt(args[1]);
+        String problema = "fitness 5,7 sin especial/tsp 5 1";//args[0];
+        double[][] dominanciaSpea = new double[corrida][corrida];
+        double[][] dominanciaCercano = new double[corrida][corrida];
+         double[][] dominanciaSpeavsSpea = new double[corrida][corrida];
+        double[][] dominanciaCercanovsCercano = new double[corrida][corrida];
+        double[][] dominanciaNsgavsNsga = new double[corrida][corrida];
+        double[][] HV = new double[3][corrida];
+        Hypervolume hyperVolumen = new Hypervolume();
+        for (int i = 0; i < corrida; i++) {
             double[][] matrizSPEA;
             double[][] matrizSPEAanillos;
-            double[][]truepareto;
-        for (int j=0;j<corrida;j++){
-        matrizSPEA = archivoAMatriz("C:/tesis/FUNspea"+i);
-        truepareto = archivoAMatriz("C:/tesis/FUN3ob");
+            double[][] matrizSPEASiguiente;
+            double[][] matrizSPEAanterior;
+            double[][] truepareto;
+               double[][] matrizNSGA;
+                double[][] matrizNSGAanterior;
+            truePareto(problema+"/SPEACercano/FUNspeaCercano_",problema+"/pareto1",corrida);
+            truePareto(problema+"/SPEA2/FUNspea2_",problema+"/pareto2",corrida);
+            truePareto(problema+"/NSGA2/FUNnsga2_",problema+"/pareto3",corrida);
+//            promedioCorrida(problema+"/SPEACercano/FUNspeaCercano_",problema+"/promCercano",corrida);
+//            promedioCorrida(problema+"/SPEA2/FUNspea2_",problema+"/promSPEA",corrida);
+//            promedioCorrida(problema+"/NSGA2/FUNnsga2_",problema+"/PromNSGA",corrida);
+//            paretoFinal(problema+"/pareto", problema+"/truepareto");
+             paretoFinal(problema+"/pareto", problema+"/truepareto0");
+//
+
+
+            for (int j = 0; j < corrida; j++) {
+                matrizSPEA = archivoAMatriz(problema + "/SPEA2/FUNspea2_" + i);
+               
+                 truepareto = archivoAMatriz(problema + "/truepareto0");
 //        double[][] matrizNSGA = archivoAMatriz("C:/tesis/NSGAIIFUN");
 //        double[][] matrizSPEAdis = archivoAMatriz("C:/tesis/SPEAFUNDISTANCIA");
 //        double[][] matrizSPEAcua = archivoAMatriz("C:/tesis/SPEAFUNCUADRANTE");
-        matrizSPEAanillos = archivoAMatriz("C:/tesis/FUNspeaCercano"+j);
-        int objetivos = objetivos("C:/tesis/FUNspea"+i);
-        SetCoveragevariasCorridas SC = new SetCoveragevariasCorridas();
-        dominanciaSpea[i][j]= SC.calcular(matrizSPEA,matrizSPEAanillos, objetivos);
-        dominanciaCercano[j][i]=SC.calcular(matrizSPEAanillos, matrizSPEA, objetivos);
-            System.out.println("");
-        HV[0][j] = toma.hypervolume(matrizSPEA,truepareto,3);
-         HV[1][j] = toma.hypervolume(matrizSPEAanillos,truepareto,3);
+                  matrizSPEASiguiente = archivoAMatriz(problema + "/SPEA2/FUNspea2_" + j);
+                    matrizNSGA = archivoAMatriz(problema+"/NSGA2/FUNnsga2_"+j);
+                    matrizNSGAanterior = archivoAMatriz(problema+"/NSGA2/FUNnsga2_"+i);
+                matrizSPEAanillos = archivoAMatriz("C:/tesis/" + problema + "/Speacercano/FUNspeacercano_" + j);
+                 matrizSPEAanterior = archivoAMatriz("C:/tesis/" + problema + "/Speacercano/FUNspeacercano_" + i);
+                int objetivos = objetivos("C:/tesis/" + problema + "/SPEA2/FUNspea2_" + i);
+                SetCoveragevariasCorridas SC = new SetCoveragevariasCorridas();
+                dominanciaSpea[i][j] = SC.calcular(matrizSPEA, matrizSPEAanillos, objetivos);
+                dominanciaCercano[j][i] = SC.calcular(matrizSPEAanillos, matrizSPEA, objetivos);
+                dominanciaSpeavsSpea[i][j] = SC.calcular(matrizSPEA, matrizSPEASiguiente, objetivos);
+                dominanciaCercanovsCercano[i][j] = SC.calcular(matrizSPEAanterior, matrizSPEAanillos, objetivos);
+                dominanciaNsgavsNsga[i][j] = SC.calcular(matrizNSGAanterior,matrizNSGA, objetivos);
+                HV[0][i] = hyperVolumen.hypervolume(matrizSPEA, truepareto, objetivos);
+                HV[1][j] = hyperVolumen.hypervolume(matrizSPEAanillos, truepareto, objetivos);
+                HV[2][j] = hyperVolumen.hypervolume(matrizNSGA, truepareto, objetivos);
 //        SC.calcular(matrizNSGA, matrizSPEAdis, objetivos);
 //        SC.calcular(matrizSPEAdis, matrizNSGA, objetivos);
 //        SC.calcular(matrizSPEAdis, matrizSPEA, objetivos);
 //        SC.calcular(matrizSPEA, matrizSPEAdis, objetivos);
 //        SC.calcular(matrizSPEAcua, matrizSPEA, objetivos);
 //        SC.calcular(matrizSPEA, matrizSPEAcua, objetivos);
-        }
-            
-        }
-        int total=0;
+            }
 
-        for (int i =0;i<corrida;i++){
+        }
+        int total = 0;
+              poblacion.printPromToFile(problema+"/dominanciaSPEAVSSPEA",dominanciaSpeavsSpea);
+               poblacion.printPromToFile(problema+"/dominanciaCercanovsCercano",dominanciaCercanovsCercano);
+                poblacion.printPromToFile(problema+"/dominanciaNsgavsNsga",dominanciaNsgavsNsga);
+              poblacion.printPromToFile(problema+"/hvspea",HV);
+        for (int i = 0; i < corrida; i++) {
             System.out.println("Dominancia Spea");
-            int count=0;
-            int count2=0;
-            for (int j =0;j<corrida;j++){
-                if (dominanciaSpea[i][j]>dominanciaCercano[j][i]){
+            int count = 0;
+            int count2 = 0;
+            for (int j = 0; j < corrida; j++) {
+                if (dominanciaSpea[i][j] > dominanciaCercano[j][i]) {
                     count++;
-                }
- else
-                {
+                } else {
                     count2++;
- }
-               
+                }
+
             }
-             System.out.println(i+ "Spea Domino a: "+count);
-                System.out.println("Spea Me dominan:" +count2);
-                if (count>count2) total++;
-                System.out.println("");
-                System.out.println("HV de spea "+i+": "+HV[0][i]);
-                System.out.println("HV de cercano "+i+": "+HV[1][i]);
+            System.out.println(i + "Spea Domino a: " + count);
+            System.out.println("Spea Me dominan:" + count2);
+            if (count > count2) {
+                total++;
+            }
+            System.out.println("");
+            System.out.println("HV de spea " + i + ": " + HV[0][i]);
+            System.out.println("HV de cercano " + i + ": " + HV[1][i]);
         }
-        System.out.println(total+" de los 10 dominan a cercano");
+        System.out.println(total + " de los 10 dominan a cercano");
         System.out.println(" ");
-        total=0;
-        for (int i =0;i<corrida;i++){
+        total = 0;
+        for (int i = 0; i < corrida; i++) {
             System.out.println("Dominancia Cercano");
-            int count=0;
-            int count2=0;
-            for (int j =0;j<corrida;j++){
-                if (dominanciaSpea[i][j]<dominanciaCercano[j][i]){
+            int count = 0;
+            int count2 = 0;
+            for (int j = 0; j < corrida; j++) {
+                if (dominanciaSpea[i][j] < dominanciaCercano[j][i]) {
                     count++;
-                }
- else
-                {
+                } else {
                     count2++;
- }
-               
+                }
+
             }
-             System.out.println(i+"Cercano Domino a: "+count);
-                System.out.println("Cercano Me dominan:" +count2);
-                System.out.println("");
-                if (count>count2) total++;
+            System.out.println(i + "Cercano Domino a: " + count);
+            System.out.println("Cercano Me dominan:" + count2);
+            System.out.println("");
+            if (count > count2) {
+                total++;
+            }
         }
-        System.out.println(total+" de cercano dominan a spea");
+        System.out.println(total + " de cercano dominan a spea");
     }
 
     public static double calcular(double[][] X, double[][] Y, int numberOfObjectives) {
@@ -126,10 +166,140 @@ public class SetCoveragevariasCorridas {
         }
         totalDom = totalDom / 100;
 
-       // System.out.println(totalDom + "%");
+        // System.out.println(totalDom + "%");
         return totalDom;
     }
 
+    public static void truePareto(String path,String pathPareto, int corridas) throws FileNotFoundException, IOException, WriteException, BiffException {
+        double[][] pareto = null;
+        FileOutputStream fos = new FileOutputStream(pathPareto);
+        OutputStreamWriter osw = new OutputStreamWriter(fos);
+        BufferedWriter bw = new BufferedWriter(osw);
+
+        for (int i = 0; i < corridas; i++) {
+            pareto = archivoAMatriz(path + i);
+//            pareto = paretoNoDominado(pareto);
+            for (int j = 0; j < pareto.length; j++) {
+                 String f = "";
+                 for (int h=0;h<pareto[j].length;h++){
+                     f = f+pareto[j][h]+" ";
+                 }
+                bw.write(f);
+                bw.newLine();
+            }
+        }
+        /* Close the file */
+        bw.close();
+
+    }
+
+    public static void promedioCorrida(String path,String pathPareto, int corridas) throws FileNotFoundException, IOException, WriteException, BiffException {
+        double[][] pareto = null;
+        FileOutputStream fos = new FileOutputStream(pathPareto);
+        OutputStreamWriter osw = new OutputStreamWriter(fos);
+        BufferedWriter bw = new BufferedWriter(osw);
+        double[][] fin = null;
+        for (int i = 0; i < corridas; i++) {
+
+            pareto = archivoAMatriz(path + i);
+              if (i==0){
+                 fin = new double[pareto.length][pareto[0].length];
+            }
+
+            for (int j = 0; j < pareto.length; j++) {
+
+                 for (int h=0;h<pareto[j].length;h++){
+                     fin[j][h] += pareto[j][h];
+                 }
+
+            }
+        }
+        /* Close the file */
+      for (int j = 0; j < fin.length; j++) {
+
+                 for (int h=0;h<fin[j].length;h++){
+                     fin[j][h] /= corridas;
+                 }
+
+            }
+            poblacion.printPromToFile(pathPareto, fin);
+    }
+  public static double[][] paretoNoDominado(double[][] X) throws WriteException, BiffException {
+        int xObj = 0;
+        double xDomy = 0.0;
+        double[] dominados = new double[X.length];
+        double totalDom = 0.0;
+
+        int cont=0;
+        for (int i = 0; i < X.length; i++) {
+            for (int k = 0; k < X.length; k++) {
+                if (i!=k){
+                for (int j = 0; j < X[0].length; j++) {
+                    double x = X[i][j];
+                    double y = X[k][j];
+                    if (x <= y) {
+                        xObj += 1;
+                    }
+                    if (xObj == X[0].length) {
+                        int sacar = 0;
+                        for (int h = 0; h < dominados.length; h++) {
+                            if (k == dominados[h]) {
+                                sacar = 1;
+                            }
+                        }
+                        if (sacar == 0) {
+                            dominados[cont] = k;
+                            cont++;
+                        }
+                    }
+                }
+                xObj = 0;
+                }
+            }
+        }
+        double[][] noDominado = new double[X.length-cont][X[0].length];
+         cont=0;
+         for (int i = 0; i < X.length; i++) {
+
+             int sacar=0;
+              for (int j= 0; j< dominados.length; j++) {
+            if (i==dominados[j])
+                sacar=1;
+        }
+             if (sacar==0){
+                 noDominado[cont]=X[i];
+                 cont++;
+             }
+         }
+   return noDominado;
+
+
+    }
+
+   public static void paretoFinal(String path,String pathPareto) throws FileNotFoundException, IOException, WriteException, BiffException {
+        double[][] pareto = null;
+        FileOutputStream fos = new FileOutputStream(pathPareto);
+        OutputStreamWriter osw = new OutputStreamWriter(fos);
+        BufferedWriter bw = new BufferedWriter(osw);
+        for (int i = 1; i <= 3; i++) {
+
+            pareto = archivoAMatriz(path + i);
+            
+            for (int j = 0; j < pareto.length; j++) {
+               String f = "";
+                 for (int h=0;h<pareto[j].length;h++){
+                     f = f+pareto[j][h]+" ";
+                 }
+                bw.write(f);
+                bw.newLine();
+            }
+        }
+        /* Close the file */
+        bw.close();
+
+//        truePareto(pathPareto, pathPareto,1);
+
+    }
     public static double[][] archivoAMatriz(String path) {
         String linea = "";
         int espacio = 0;
@@ -146,8 +316,8 @@ public class SetCoveragevariasCorridas {
                 String[] individuo = linea.split(" ");
 
                 for (int i = 0; i < objetivos; i++) {
-                   
-                    
+
+
                     matrizRetorno[contador][i] = Double.parseDouble(individuo[i]);
                 }
                 contador++;
@@ -155,9 +325,8 @@ public class SetCoveragevariasCorridas {
         } catch (Exception ex) {
             Logger.getLogger(SPEA2_main.class.getName()).log(Level.SEVERE, null, ex);
         }
-        return matrizRetorno;
-
-
+      
+return matrizRetorno;
     }
 
     public static int contadorPoblacion(String path) {
